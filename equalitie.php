864836ccdf1d826b71344b0e3b95d7e36731df0f
uwantsomecandy <djs_scheenloop@hotmail.com>
2014-09-10 14:42:19 +0200

    even-oneven

---
+++ equalitie.php
@@ -0,0 +1,15 @@
+
+
+
+<?php	
+	for ($getal = 0; $getal <= 100; $getal++) {
+
+	if ($getal % 2 == 0) {
+		echo $getal . " is even<br>";
+	}
+	elseif ($getal % 1 == 0 ) {
+		echo $getal . " is oneven<br>";
+	}
+}
+
+?>
